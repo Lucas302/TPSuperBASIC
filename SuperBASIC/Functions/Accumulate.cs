a1876fbe6fb8be7beb82c1521075959e923e9e6c
JLS93 <jlsvbn@gmail.com>
2022-06-22 15:39:00 +0200

    Add Accumulate.cs in functions and in Program.cs

---
+++ SuperBASIC/Functions/Accumulate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBASIC.Functions
+{
+    class Accumulate : IFunction
+    {
+        float a, b;
+        float sum = 0;
+        public float Apply(List<BasicNumber> arguments)
+        {
+
+            a = Memory.MemoryGet((short)arguments[0].GetValue());
+            b = Memory.MemoryGet((short)arguments[1].GetValue());
+
+            if (a < b)
+            {
+                for (var i = a; i < b+1; i++)
+                {
+                    sum = sum + i;
+                }
+            } else
+            {
+                return float.NaN;
+            }
+            return sum;
+        }
+    }
+}
