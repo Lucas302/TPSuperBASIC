1ff59f8c3c6c82a2701c6f93ae6eb1fea3436453
JLS93 <jlsvbn@gmail.com>
2022-06-22 09:24:30 +0200

    Add Sin in functions and in Program.cs

---
+++ SuperBASIC/Functions/Sin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBASIC.Functions
+{
+    class Sin : IFunction
+    {
+        public float Apply(List<BasicNumber> arguments)
+        {
+            return (float)Math.Sin(arguments[0]);
+        }
+    }
+}
