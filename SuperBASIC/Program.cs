fe8b889c713e41b56e9ac3015ddcdafa0c795540
AxSolution <axel733@orange.fr>
2022-06-22 10:36:12 +0200

    ajout volume sphere

--- SuperBASIC/Program.cs
+++ SuperBASIC/Program.cs
@@ -26,6 +26,8 @@ namespace SuperBASIC
 				lib.AddFunction(new Functions.ASin(), 1, "ASIN");
 				lib.AddFunction(new Functions.Sin(), 1, "SIN");
 				lib.AddFunction(new Functions.Rem(), 2, "REM");
+				lib.AddFunction(new Functions.SphereVol(), 1, "SPHERE_VOL");
+
 
 				Runtime r = new Runtime(lib);
 				if (args.Length <= 1)
