a411c6868ab57fa8f328c5c4448e8e838388ed78
JLS93 <jlsvbn@gmail.com>
2022-06-22 11:46:48 +0200

    Add VEC3ADD.cs in functions and in Program.cs

---
+++ SuperBASIC/Functions/SphereVol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBASIC.Functions
+{
+    class SphereVol : IFunction
+    {
+        public float Apply(List<BasicNumber> arguments)
+        {
+            return (float) (Math.PI * Math.Pow(arguments[0].GetValue(), 3) / 3);
+        }
+    }
+}
