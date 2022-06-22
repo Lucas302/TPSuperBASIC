0ab6c7f9588788724b8cd6358070d63a9a11b219
MichelRacaud <mracaud@wanadoo.fr>
2022-06-22 15:54:59 +0200

    optimisation OrthogonalBBox

--- SuperBASIC/Functions/OrthogonalBBox.cs
+++ SuperBASIC/Functions/OrthogonalBBox.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SuperBASIC.Functions
 {
+    public struct Point { public float x; public float y; public float z; }
     public class OrthogonalBBox : IFunction
     {
+        static (Point, Point) CalculateBox(List<Point> points)
+        {
+            Point petit;
+            Point grand;
+
+            petit = grand = points.First();
+
+            foreach(var pt in points.Skip(1))
+            {
+                petit.x = Math.Min(pt.x, petit.x);
+                petit.y = Math.Min(pt.y, petit.y);
+                petit.z = Math.Min(pt.z, petit.z);
+            }
+
+            foreach (var pt in points.Skip(1))
+            {
+                grand.x = Math.Max(pt.x, grand.x);
+                grand.y = Math.Max(pt.y, grand.y);
+                grand.z = Math.Max(pt.z, grand.z);
+            }
+
+            return (petit, grand);
+        }
+
         float IFunction.Apply(List<BasicNumber> arguments)
         {
             float a = arguments[0];
@@ -21,27 +47,47 @@ namespace SuperBASIC.Functions
             }
             else
             {
-                float minx = Memory.MemoryGet(inta);
-                float miny = Memory.MemoryGet((short)(inta + 1));
-                float minz = Memory.MemoryGet((short)(inta + 2));
-                float maxx = Memory.MemoryGet(inta);
-                float maxy = Memory.MemoryGet((short)(inta + 1));
-                float maxz = Memory.MemoryGet((short)(inta + 2));
-                for (int i = 1; i < ((intb - inta + 1) / 3); i++)
+                var listepoints = new List<Point>();
+                for (int i = 0; i < ((intb - inta + 1) / 3); i++)
                 {
-                    if (Memory.MemoryGet((short)(inta + 3 * i)) < minx) { minx = Memory.MemoryGet((short)(inta + 3 * i)); }
-                    if (Memory.MemoryGet((short)(inta + 3 * i + 1)) < miny) { miny = Memory.MemoryGet((short)(inta + 3 * i + 1)); }
-                    if (Memory.MemoryGet((short)(inta + 3 * i + 2)) < minz) { minz = Memory.MemoryGet((short)(inta + 3 * i + 2)); }
-                    if (Memory.MemoryGet((short)(inta + 3 * i)) > maxx) { maxx = Memory.MemoryGet((short)(inta + 3 * i)); }
-                    if (Memory.MemoryGet((short)(inta + 3 * i + 1)) > maxy) { maxy = Memory.MemoryGet((short)(inta + 3 * i + 1)); }
-                    if (Memory.MemoryGet((short)(inta + 3 * i + 2)) > maxz) { maxz = Memory.MemoryGet((short)(inta + 3 * i + 2)); }
+                    var point = new Point();
+                    point.x = Memory.MemoryGet((short)(inta + 3 * i));
+                    point.y = Memory.MemoryGet((short)(inta + 3 * i + 1));
+                    point.z = Memory.MemoryGet((short)(inta + 3 * i + 2));
+                    listepoints.Add(point);
                 }
-                Memory.MemorySet(intd, minx);
-                Memory.MemorySet((short)(intd + 1), miny);
-                Memory.MemorySet((short)(intd + 2), minz);
-                Memory.MemorySet((short)(intd + 3), maxx);
-                Memory.MemorySet((short)(intd + 4), maxy);
-                Memory.MemorySet((short)(intd + 5), maxz);
+
+                var (petit, grand) = CalculateBox(listepoints);
+
+                Memory.MemorySet(intd, petit.x);
+                Memory.MemorySet((short)(intd + 1), petit.y);
+                Memory.MemorySet((short)(intd + 2), petit.z);
+                Memory.MemorySet((short)(intd + 3), grand.x);
+                Memory.MemorySet((short)(intd + 4), grand.y);
+                Memory.MemorySet((short)(intd + 5), grand.z);
+
+                /*              float minx = Memory.MemoryGet(inta);
+                                float miny = Memory.MemoryGet((short)(inta + 1));
+                                float minz = Memory.MemoryGet((short)(inta + 2));
+                                float maxx = Memory.MemoryGet(inta);
+                                float maxy = Memory.MemoryGet((short)(inta + 1));
+                                float maxz = Memory.MemoryGet((short)(inta + 2));
+                                for (int i = 1; i < ((intb - inta + 1) / 3); i++)
+                                {
+                                    if (Memory.MemoryGet((short)(inta + 3 * i)) < minx) { minx = Memory.MemoryGet((short)(inta + 3 * i)); }
+                                    if (Memory.MemoryGet((short)(inta + 3 * i + 1)) < miny) { miny = Memory.MemoryGet((short)(inta + 3 * i + 1)); }
+                                    if (Memory.MemoryGet((short)(inta + 3 * i + 2)) < minz) { minz = Memory.MemoryGet((short)(inta + 3 * i + 2)); }
+                                    if (Memory.MemoryGet((short)(inta + 3 * i)) > maxx) { maxx = Memory.MemoryGet((short)(inta + 3 * i)); }
+                                    if (Memory.MemoryGet((short)(inta + 3 * i + 1)) > maxy) { maxy = Memory.MemoryGet((short)(inta + 3 * i + 1)); }
+                                    if (Memory.MemoryGet((short)(inta + 3 * i + 2)) > maxz) { maxz = Memory.MemoryGet((short)(inta + 3 * i + 2)); }
+                                }
+                                Memory.MemorySet(intd, minx);
+                                Memory.MemorySet((short)(intd + 1), miny);
+                                Memory.MemorySet((short)(intd + 2), minz);
+                                Memory.MemorySet((short)(intd + 3), maxx);
+                                Memory.MemorySet((short)(intd + 4), maxy);
+                                Memory.MemorySet((short)(intd + 5), maxz);
+                */
                 return 0;
             }
         }
