a70f1c2226d70da0714f48c25c44ef8e71738501
Lucas Dupouy <dupouy.lucas@gmail.com>
2022-06-22 10:14:27 +0200

    Test Cos

--- TestsSuperBASIC/PrebuiltTest.cs
+++ TestsSuperBASIC/PrebuiltTest.cs
@@ -33,5 +33,23 @@ namespace TestsSuperBASIC
 				Assert.AreEqual(expected, received);
             }
 		}
-    }
+		[TestMethod]
+		public void TestCos()
+        {
+			Library lib = new Library();
+			var printer = new Mock.MockPrint();
+			lib.AddFunction(printer, 1, "PRINT");
+
+			lib.AddFunction(new SuperBASIC.Functions.MemoryStore(), 2, "MEMSTORE");
+			lib.AddFunction(new SuperBASIC.Functions.Cos(), 1, "COS");
+			Runtime r = new Runtime(lib);
+			r.OpenFile(Directory.GetCurrentDirectory() + "\\CasDeTest\\TestCos.basic");
+			r.Run();
+			var expected = 0.8414709848;
+			var received = printer.output[0];
+			double delta = 0.001;
+			Assert.AreEqual(expected, received, delta);
+
+		}
+	}
 }
