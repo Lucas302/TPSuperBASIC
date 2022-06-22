82e717b31d010e867a7acc53aafc3c747585a9c8
JLS93 <jlsvbn@gmail.com>
2022-06-22 09:22:33 +0200

    Add ASin in function and in Program.cs

---
+++ SuperBASIC/Functions/ASin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBASIC.Functions
+{
+    public class ASin : IFunction
+    {
+        public float Apply(List<BasicNumber> arguments)
+        {
+            return (float)Math.Asin(arguments[0]);
+        }
+    }
+}
