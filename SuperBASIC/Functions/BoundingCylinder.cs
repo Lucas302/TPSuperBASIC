04e3ea044f6693b86fd0941c343a254c235d632b
MichelRacaud <mracaud@wanadoo.fr>
2022-06-23 09:25:43 +0200

    ajout commentaires dans BoundingCylinder

--- SuperBASIC/Functions/BoundingCylinder.cs
+++ SuperBASIC/Functions/BoundingCylinder.cs
@@ -21,6 +21,7 @@ namespace SuperBASIC.Functions
             }
             else
             {
+                // on met les points dans une liste de points
                 var listepoints = new List<Point>();
                 for (int i = 0; i < ((intb - inta + 1) / 3); i++)
                 {
@@ -30,10 +31,21 @@ namespace SuperBASIC.Functions
                     point.z = Memory.MemoryGet((short)(inta + 3 * i + 2));
                     listepoints.Add(point);
                 }
+                // petit est le point le plus bas, coordonnées minx miny minz
+                // grand est le point le plus haut, coordonnées maxx maxy maxz
                 var petit = new Point();
                 var grand = new Point();
                 (petit, grand) = OrthogonalBBox.CalculateBox(listepoints);
 
+                // dans le plan z=0, les droites x=minx x=maxx y=miny y=maxy délimitent un rectangle
+                // de longueur et largeur (maxx - minx) et (maxy - miny)
+                // le but est de chercher le plus petit cercle qui contient toutes les projections des points dans le plan z=0
+                // mais l'algo est compliqué, je me contente du cercle pas minimal qui passe par les 4 sommets du rectangle
+                // centre du cercle = centre du rectangle
+                // diamètre du cercle = diagonale du rectangle
+                // rayon du cercle = hypothénuse du triangle rectangle d'autres cotés (maxx-minx)/2 et (maxy-miny)/2
+                // d'où la formule tirée du théorème de Pythagore
+                // enfin pour passer au cylindre, il a pour base le cercle dans le plan z=zmin, et pour hauteur zmax-zmin
                 Memory.MemorySet(intd, (petit.x + grand.x) / 2);
                 Memory.MemorySet((short)(intd + 1), (petit.y + grand.y) / 2);
                 Memory.MemorySet((short)(intd + 2), petit.z);
